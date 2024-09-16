c776eb55c3129b767180876e8bfa1801ea280641
centinna-l <jerryjoy99@gmail.com>
2024-09-16 12:11:48 -0400

    Slot Machine App

---
+++ Shapes App/Shapes_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shapes_AppApp.swift
+//  Shapes App
+//
+//  Created by Jerry Joy on 2024-09-16.
+//
+
+import SwiftUI
+
+@main
+struct Shapes_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
